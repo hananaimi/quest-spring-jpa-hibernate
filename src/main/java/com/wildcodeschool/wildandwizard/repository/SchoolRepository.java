50716d07a5a6ef0315f320180e822b29ef5df9b9
Hana Naimi <naimihanna@gmail.com>
2023-06-21 11:22:31 +0200

    challenge

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,10 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-// TODO : update this interface
+import com.wildcodeschool.wildandwizard.entity.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-public interface SchoolRepository {
+// TODO : update this interface
+@ResponseBody
+public interface SchoolRepository extends JpaRepository<School, Long> {
 }
